8e62be04946813bffcfacc91e4dc8c28476b3db8
Craig Paterson <craig@widdershins.co.za>
2016-01-12 22:59:02 +0200

    even simpler

--- elixir/simpler.exs
+++ elixir/simpler.exs
@@ -5,5 +5,5 @@ fizzbuzz = fn
   (_, _, n) -> n
 end
 
-Enum.map(1..100, &fizzbuzz.(rem(&1, 5), rem(&1, 3), &1)) |> Enum.each(&IO.puts/1)
+Enum.map(1..100, &fizzbuzz.(rem(&1, 5), rem(&1, 3), &1)) |> Enum.join("\n") |> IO.puts
 
